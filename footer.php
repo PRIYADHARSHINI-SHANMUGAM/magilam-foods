b499bf8ed6ede7c6ce8137f2b1d78cee8291f49b
RITHIKA-B <90852215+RITHIKA-B@users.noreply.github.com>
2023-05-10 11:47:38 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,50 @@
+<footer class="footer" >
+
+   <section class="grid" style = "background-color : white;">
+   
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:magilam@gmail.com">magilam@gmail.com</a>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>12:00 to 23:00</p>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Andavar street Edayankatuvalasu Erode-638001 </a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:9787099399">9787099399</a>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Magilam restaurant</span> </div>
+
+</footer>
+
